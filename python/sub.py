8f7be5a3770744c8b2c1e011ded948e2827ba0ea
user <jwhsueh@ubuntu.(none)>
2015-04-21 22:26:53 -0700

    add opt procedure

--- python/sub.py
+++ python/sub.py
@@ -1,12 +1,15 @@
 from astropy.io import fits
 
-hdulist=fits.open('./glafic/B1555_try6.fits')
+#hdulist=fits.open('./glafic/out_image.fits')
+hdulist=fits.open('model_convolved.fits')
 model=hdulist[0].data
 
-hdulist2=fits.open('./B1555/B1555_nirc2_n_Kp_6x6.fits')
+#hdulist2=fits.open('./B1555/B1555_nirc2_n_Kp_6x6.fits')
+hdulist2=fits.open('B1555_cutout_120x120pix.fits')
+
 image=hdulist2[0].data
 
 res=image-model
 
 hdu=fits.PrimaryHDU(res)
-hdu.writeto('residue.fits')
+hdu.writeto('Simona_residue.fits')
