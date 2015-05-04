d3e99e01e57ff14a3fa0c9576c6cfddb46fcf921
jwhsueh <jwhsueh>
2015-05-04 15:45:20 -0700

    update MCMC plot code

---
+++ python/MCMC_full_plot.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpat
+
+t=np.loadtxt('SIE2_free_mcmc.dat')
+
+
+chi2=t[:,0]
+sig1,sig2=t[:,1],t[:,6]
+x1,x2=t[:,2],t[:,7]
+y1,y2=t[:,3],t[:,8]
+e1,e2=t[:,4],t[:,9]
+pa1,pa2=t[:,5],t[:,10]
+
+#pick a parameter to plot
+para1=sig1
+n_para='sigma1'
+tot='total counts = %d' %(len(chi2))
+
+plt.figure(1)
+n, bin, patches = plt.hist(para1,50,label=tot)
+plt.xlabel(n_para)
+plt.ylabel('counts')
+
+print len(chi2)
+
+#plt.figure(2)
+#plt.scatter(para1,chi2)
+#plt.xlabel(n_para)
+#plt.ylabel('chi^2')
+plt.legend()
+plt.show()
+
+
