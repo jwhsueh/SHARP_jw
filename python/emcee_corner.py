c4c01555ae7a1434591c368990391552b92fcf99
user <jwhsueh@ubuntu.(none)>
2015-05-08 12:00:48 -0700

    some bug fix

---
+++ python/emcee_corner.py
@@ -0,0 +1,47 @@
+import numpy as np
+import triangle
+import matplotlib.pyplot as plt
+#import matplotlib.patches as mpat
+
+file_name=['chain.dat']
+
+k=0
+for i in file_name:
+    temp=np.loadtxt(i)
+    if k==0:
+        t=temp
+
+    else:
+        t=np.append(t,temp,axis=0)
+
+    k=k+1
+
+
+
+#chi2=t[:,0]
+#sig1,sig2=t[:,1],t[:,6]
+#x1,x2=t[:,2],t[:,7]
+#y1,y2=t[:,3],t[:,8]
+#e1,e2=t[:,4],t[:,9]
+#pa1,pa2=t[:,5],t[:,10]
+
+data=t[:,:10] #ignore source posotions first
+#print len(chi2)
+
+
+## triangle plot
+
+#ndim, nsamples = 10, len(chi2)
+
+figure = triangle.corner(data, labels=[r"$\sigma 1$", r"$x1$", r"$y1$",
+                                       r"$e1$",r"$PA1$",
+                                       r"$\sigma 2$", r"$x2$", r"$y2$",
+                                       r"$e2$",r"$PA2$"],
+                         
+                         quantiles=[0.16, 0.5, 0.84],
+                         show_titles=True, title_args={"fontsize": 12})
+figure.savefig("test.png")
+
+
+
+
