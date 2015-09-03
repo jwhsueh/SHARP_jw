4775756737253b3b68679bb24a26a7f1a76d9454
cdfassnacht <cdfassnacht@ucdavis.edu>
2015-09-02 18:00:41 -0700

    Changes to plot_B1555_radio_ir in prep for 2-contour plot

--- python/plot_B1555_radio_ir.py
+++ python/plot_B1555_radio_ir.py
@@ -2,21 +2,39 @@
 A program to generate figure 2 in the B1555 edge-on disk paper.
 This figure shows the Keck AO image as greyscale, and then has contours
 that show the MERLIN emission.
+
+Typical output file name: 1555_ao_merlin_overlay.eps
 """
 
 import imfuncs as imf
 from matplotlib import pyplot as plt
+import sys
 
 """ Set the desired output """
-output = 'eps'
-outname = '1555_ao_merlin_overlay'
+if len(sys.argv)>1:
+    outname = sys.argv[1]
+else:
+    outname = None
+
+""" Set the input file names """
+aoim      = 'B1555_nirc2_n_Kp_6x6.fits'
+merlin_im = '1555_merlin_5ghz.fits'
+vlbi_im   = 'B1555_NA_SELFCAL.FITS'
+
+""" Hardwire rms levels if needed """
+rms_vlbi = 0.0001
+
+""" Set the image center and size """
+racent  = 239.29968
+deccent = 37.359921
+imsize  = 1.2       # Value in arcsec
 
 """ Set the figure size """
 plt.figure(figsize=(5.7,5.7))
 
 """ Make the overlay plot """
-imf.overlay_contours('B1555_nirc2_n_Kp_6x6.fits','1555_merlin_5ghz.fits',
-                     239.29968,37.359921,1.2,showradec=False,sighigh=6.)
+imf.overlay_contours(merlin_im,vlbi_im,racent,deccent,imsize,rms2=rms_vlbi,
+                     showradec=False,sighigh=6.)
 
 """ Set up the font """
 if plt.get_backend() == 'MacOSX':
@@ -43,10 +61,10 @@ plt.text(-0.25,0.25,'C',fontdict=font)
 plt.text(0.16,-0.24,'D',fontdict=font)
 
 """ Show/save the figure """
-if output == 'eps':
-    plt.savefig('%s.eps' % outname)
+if outname:
+    plt.savefig('%s' % outname)
     print ''
-    print 'Saved figure to %s.eps' % outname
+    print 'Saved figure to %s' % outname
     print ''
 else:
     plt.show()
