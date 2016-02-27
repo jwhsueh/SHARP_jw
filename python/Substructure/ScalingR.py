a6df9693b38112d71fd663b0caf48feb498ad40e
jwhsueh <jwhsueh>
2016-02-26 23:02:18 -0800

    scale length code half done

--- python/Substructure/ScalingR.py
+++ python/Substructure/ScalingR.py
@@ -32,29 +32,26 @@ sig = np.sqrt(sig)
 
 ## M200, c200 & scale length
 
-Hz = 100*cosmopara.h*distance.Ez(cosmopara,zl)
-print Hz
+Hz = 100*cosmopara.h*distance.Ez(cosmopara,zl) # km/s/Mpc
+Hz = Hz*1e3/3.08e22 # s-1
+#print Hz
 #Hz = distance.Ez(cosmopara,zl) # h
 
-M_200 = sig**3*(G/100/Hz**2)**(0.5)*(2/G)**(3/2) # kg
-M_200 = M_200/2e30 # M_sun
+#M_200 = sig**3*(G/100/Hz**2)**(0.5)*(2/G)**(3/2) # kg
+M_200 = sig**3*(G/Hz**2)**(0.5)*(2/G)**(3./2.) # kg
+M_200_sun = M_200/2e30 # M_sun
 
-c_200 = 5.71*(M_200/2e12*cosmopara.h)**(-0.084)*(1+zl)**(-0.47) # no unit
+print M_200_sun
+
+c_200 = 5.71*(M_200_sun/2e12*cosmopara.h)**(-0.084)*(1+zl)**(-0.47) # no unit
 
 print c_200
 
-rho_c = 3*Hz**2/(8*np.pi*G) # h^2
+rho_c = 3*Hz**2/(8*np.pi*G) # kg/m^3
 
-print rho_c
-
-#rs = (3*M_200/(800*np.pi*c_200**3))#**(1/3) # m * h^-1
-rs = (3*M_200/800/np.pi/c_200**3)
+rs = (3*M_200/8/np.pi/rho_c/c_200**3)**(1.0/3.0) # m * h^-1
+rs = rs/3.08e22/cosmopara.h/100 # convert to Mpc
 print rs
-rs = rs/3.08e22 # Mpc
 rs = np.degrees(rs/Dl)*3600 # arcsec 
 
-#print rs
-
-
-
-
+print rs
