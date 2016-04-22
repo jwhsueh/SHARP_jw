2d42e7f081152a418d9d418319e964a1ab1e6efa
jwhsueh <jwhsueh>
2016-04-22 14:52:47 -0700

    update bulge list

--- python/Substructure/dan_crosscheck.py
+++ python/Substructure/dan_crosscheck.py
@@ -10,7 +10,8 @@ mlow = 1e12
 mhigh = 1e14
 
 Dan = np.loadtxt('JenWei_table_disk_in_x.dat')
-Frac = np.loadtxt('CircAbove07Frac_04.txt')
+#Frac = np.loadtxt('CircAbove07Frac_01.txt')
+Frac = np.loadtxt('beta_02.txt')
 
 mID = Dan[:,0] # morphology ID
 fID = Frac[:,0] # dynamics ID
@@ -42,6 +43,7 @@ circFile = h5py.File(basePath+'/stellar_circs.hdf5')
 SubfindID = circFile[Snapshot_num]['SubfindID']
 
 CircAbove07Frac = circFile[Snapshot_num]['CircAbove07Frac']
+beta = circFile[Snapshot_num]['CircTwiceBelow0Frac']
 
 m_frac = np.zeros(len(mID))
 f_frac = np.zeros(len(fID))
@@ -57,22 +59,46 @@ gID = list(galaxyID)
 m_mass = np.zeros(len(mID))
 f_mass = np.zeros(len(fID))
 
+
+
 j = 0
 for element in mID:
-	mIndex = gID.index(element)
+	#mIndex = gID.index(element)
 	mIndex2 = sID.index(element)
 	#print sIndex
-	m_mass[j] = SubhaloMass[mIndex]
-	m_frac[j] = CircAbove07Frac[mIndex2]
+	m_mass[j] = SubhaloMass[element]
+	#m_frac[j] = CircAbove07Frac[mIndex2]
+	m_frac[j] = beta[mIndex2]
 	j = j+1
 
 j = 0
 for element in fID:
-	fIndex = gID.index(element)
+	#fIndex = gID.index(element)
 	fIndex2 = sID.index(element)
 	#print sIndex
-	f_mass[j] = SubhaloMass[fIndex]
-	f_frac[j] = CircAbove07Frac[fIndex2]
+	f_mass[j] = SubhaloMass[element]
+	#g_mass[j] = GroupMass[fIndex]
+	#f_frac[j] = CircAbove07Frac[fIndex2]
+	f_frac[j] = beta[fIndex2]
+	j = j+1	
+
+mID = set(mID)
+fID = set(fID)
+
+overlap = fID.intersection(mID)
+
+o_mass = np.zeros(len(overlap))
+o_frac = np.zeros(len(overlap))
+
+j = 0
+for element in overlap:
+	#fIndex = gID.index(element)
+	oIndex2 = sID.index(element)
+	#print sIndex
+	o_mass[j] = SubhaloMass[element]
+	#g_mass[j] = GroupMass[fIndex]
+	#o_frac[j] = CircAbove07Frac[oIndex2]
+	o_frac[j] = beta[oIndex2]
 	j = j+1	
 
 #m_mass = m_mass[m_mass>1e12]
@@ -81,16 +107,18 @@ for element in fID:
 #m_frac = m_frac[m_mass<1e14]
 
 m_mass = np.array(np.log10(m_mass))
-f_mass = np.log10(f_mass)
+f_mass = np.array(np.log10(f_mass))
+o_mass = np.array(np.log10(o_mass))
 
-print m_mass[0:10]
-print m_frac[0:10]
+print f_mass[0:10]
+#print m_frac[0:10]
 
-#plt.xlim(12.,14.)
-plt.scatter(m_mass[0:10],m_frac[0:10],'r')#,label = 'morphology')
-#plt.scatter(f_mass,f_frac)#,label = 'f>0.4')
+plt.xlim(12.,14.)
+plt.plot(m_mass,m_frac,'+',label = 'morphology')
+plt.plot(f_mass,f_frac,'r+',label = 'beta<0.2')
+plt.plot(o_mass,o_frac,'o',mec ='g',mfc = 'none',label = 'overlap (beta<0.2)')
 plt.xlabel('log10(Mass)')
-plt.ylabel('Disk star fraction')
-#plt.legend()
+plt.ylabel('Bulge star fraction (beta)')
+plt.legend()
 
 plt.show()
--- python/Substructure/illustris_circ.py
+++ python/Substructure/illustris_circ.py
@@ -17,8 +17,7 @@ circFile = h5py.File(basePath+'/stellar_circs.hdf5')
 SubfindID = circFile[Snapshot_num]['SubfindID']
 
 CircAbove07Frac = circFile[Snapshot_num]['CircAbove07Frac']
-
-
+beta = circFile[Snapshot_num]['CircTwiceBelow0Frac']
 
 
 ## cross check w/ GroupFirstSub
@@ -45,6 +44,7 @@ galaxyID = galaxyID[GroupMass<mhigh]
 ''' select of subhalo complete '''
 
 dStarFrac = np.zeros(len(galaxyID))
+bStarFrac = np.zeros(len(galaxyID))
 
 gID = list(galaxyID)
 sID = list(SubfindID)
@@ -56,6 +56,7 @@ for element in gID:
 	sIndex = sID.index(element)
 	#print sIndex
 	dStarFrac[j] = CircAbove07Frac[sIndex]
+	bStarFrac[j] = beta[sIndex]
 	j = j+1
 	
 
@@ -79,18 +80,18 @@ theta_e = distance.EinsteinR(cosmopara,zl,zs,sigma)
 
 ''' select disk galaxy (different threshhold) '''
 
-disk_e1 = theta_e[dStarFrac>0.2]
-disk_ID1 = galaxyID[dStarFrac>0.2]
+disk_e1 = theta_e[bStarFrac<0.6]
+disk_ID1 = galaxyID[bStarFrac<0.6]
 
-disk_e2 = theta_e[dStarFrac>0.4]
-disk_ID2 = galaxyID[dStarFrac>0.4]
+disk_e2 = theta_e[bStarFrac<0.4]
+disk_ID2 = galaxyID[bStarFrac<0.4]
 
-disk_e3 = theta_e[dStarFrac>0.1]
-disk_ID3 = galaxyID[dStarFrac>0.1]
+disk_e3 = theta_e[bStarFrac<0.2]
+disk_ID3 = galaxyID[bStarFrac<0.2]
 
-cat1 = open('CircAbove07Frac_02.txt','w')
-cat2 = open('CircAbove07Frac_04.txt','w')
-cat3 = open('CircAbove07Frac_01.txt','w')
+cat1 = open('beta_06.txt','w')
+cat2 = open('beta_04.txt','w')
+cat3 = open('beta_02.txt','w')
 
 files = [cat1,cat2,cat3]
 IDs = [disk_ID1,disk_ID2,disk_ID3]
@@ -126,7 +127,9 @@ disk_hist2 = disk_hist2[0]
 
 disk_hist3 = np.histogram(disk_e3,bins = np.linspace(0.,2.,41))
 disk_hist3 = disk_hist3[0]
-#print disk_hist
+#print disk_hist1
+#print disk_hist2
+#print disk_hist3
 
 fraction1 = np.zeros(len(total_hist))
 fraction2 = np.zeros(len(total_hist))
@@ -147,12 +150,12 @@ for i in range(len(total_hist)):
 
 #print fraction
 
-plt.plot(np.linspace(0.,2.,40),fraction1,label = 'f>0.2')
-plt.plot(np.linspace(0.,2.,40),fraction2,'r',label = 'f>0.4')
-plt.plot(np.linspace(0.,2.,40),fraction3,'g--',label = 'f>0.1')
+plt.plot(np.linspace(0.,2.,40),fraction1,label = 'beta<0.6')
+plt.plot(np.linspace(0.,2.,40),fraction2,'r',label = 'beta<0.4')
+plt.plot(np.linspace(0.,2.,40),fraction3,'g--',label = 'beta<0.2')
 plt.legend()
 plt.xlim(0,1.2)
 plt.xlabel('Einstein radius (arc sec)')
-plt.ylabel('disk fraction')
+plt.ylabel('disk galaxy fraction')
 plt.show()
 
